3b216049c24fc29d036747d652e1faf493ccd770
pvt2211 <48007515+pvt2211@users.noreply.github.com>
2019-12-23 18:17:39 +0700

    sign-up

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,5 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  def is_logged_in?
-    !session[:user_id].nil?
-  end
+  # Add more helper methods to be used by all tests here...
 end
